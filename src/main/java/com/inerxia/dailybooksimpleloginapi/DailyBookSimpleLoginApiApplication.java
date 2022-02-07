a702a98bf21423fc0f74c94fdc33ef4cd5293e53
QuantumInformatik <juanguillermoalarcon@gmail.com>
2022-02-06 20:17:04 -0500

    First login

---
+++ src/main/java/com/inerxia/dailybooksimpleloginapi/DailyBookSimpleLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.inerxia.dailybooksimpleloginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBookSimpleLoginApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyBookSimpleLoginApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inerxia/dailybooksimpleloginapi/DailyBookSimpleLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerxia.dailybooksimpleloginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBookSimpleLoginApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
